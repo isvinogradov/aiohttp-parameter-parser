2b7927b1871ea27b504cb3cd48b4d80ee0614696
Ivan Vinogradov <isvinogradov@gmail.com>
2020-11-19 20:38:28 +0300

    feat: bump lib version in setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='aiohttp-parameter-parser',
-    version='0.1.4',
+    version='0.1.5',
     description='Declare and validate HTTP query and path parameters in aiohttp',
     long_description=long_description,
     long_description_content_type="text/markdown",
